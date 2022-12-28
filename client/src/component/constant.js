67e02c886f9fe8cb0f6744572ee937cd5b27b6d3
LaughingThi00 <92248229+LaughingThi00@users.noreply.github.com>
2022-12-28 22:05:32 +0700

    OK all

--- client/src/component/constant.js
+++ client/src/component/constant.js
@@ -1,7 +1,7 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:5000/api'
-		: 'https://server-render-2.onrender.com'
+		: 'https://server-render-2.onrender.com/api'
 
 export const clientUrl = process.env.NODE_ENV !== 'production'
 		? 'http://localhost:3000'
