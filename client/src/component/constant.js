5778c0fb83c1aefbd5df891f67fa9b4e56241ce7
LaughingThi00 <92248229+LaughingThi00@users.noreply.github.com>
2022-12-28 21:37:18 +0700

    OK for deploy

--- client/src/component/constant.js
+++ client/src/component/constant.js
@@ -1,7 +1,7 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:5000/api'
-		: 'https://interation-project-221.vercel.app'
+		: 'https://server-render-2.onrender.com'
 
 export const clientUrl = process.env.NODE_ENV !== 'production'
 		? 'http://localhost:3000'
